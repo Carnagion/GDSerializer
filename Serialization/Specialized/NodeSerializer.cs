496bdd20a914aa830d206bb006bf061402b0c213
Carnagion <carnagion.games@gmail.com>
2022-08-02 08:27:32 +0200

    Refactor node serializer to use member (de)serialization methods

--- Serialization/Specialized/NodeSerializer.cs
+++ Serialization/Specialized/NodeSerializer.cs
@@ -1,96 +1,100 @@
 using System;
 using System.Linq;
+using System.Reflection;
 using System.Xml;
 
 using Godot.Utility.Extensions;
 
 namespace Godot.Serialization.Specialized
 {
-    /// <summary>
-    /// A (de)serializer for <see cref="Node"/> and types that inherit from it.
-    /// </summary>
-    public class NodeSerializer : CollectionSerializer
+    internal class NodeSerializer : CollectionSerializer
     {
-        /// <summary>
-        /// Initialises a new <see cref="NodeSerializer"/> with the specified parameters.
-        /// </summary>
-        /// <param name="defaultSerializer">The serializer to use when (de)serializing the <see cref="Node"/>'s fields and properties.</param>
-        public NodeSerializer(ISerializer defaultSerializer) : base(defaultSerializer)
+        public NodeSerializer(Serializer defaultSerializer) : base(defaultSerializer)
         {
+            this.defaultSerializer = defaultSerializer;
         }
         
-        /// <summary>
-        /// Serializes <paramref name="instance"/> into an <see cref="XmlNode"/>.
-        /// </summary>
-        /// <param name="instance">The <see cref="object"/> to serialize. It must be or inherit from <see cref="Node"/>.</param>
-        /// <param name="nodeType">The <see cref="Type"/> to serialize <paramref name="instance"/> as.</param>
-        /// <returns>An <see cref="XmlNode"/> that represents <paramref name="instance"/> and the serializable data stored in it.</returns>
-        /// <exception cref="SerializationException">Thrown if <paramref name="instance"/> could not be serialized due to unexpected errors or invalid input.</exception>
+        private readonly Serializer defaultSerializer;
+        
         public override XmlNode Serialize(object instance, Type? nodeType = null)
         {
             nodeType ??= instance.GetType();
-            if (instance is not Node node)
+            if (!typeof(Node).IsAssignableFrom(nodeType))
             {
                 throw new SerializationException(instance, $"\"{nodeType.GetDisplayName()}\" cannot be (de)serialized by {typeof(NodeSerializer).GetDisplayName()}");
             }
             
-            XmlNode element = this.ItemSerializer.Serialize(instance, nodeType);
+            XmlDocument context = new();
             
-            try
+            // Use the "Type" attribute if generic or nested type as ` and + are not allowed as XML node names
+            XmlElement element;
+            if (nodeType.IsGenericType)
             {
-                if (node.GetChildCount() is 0)
-                {
-                    return element;
-                }
-                XmlDocument context = element.OwnerDocument!;
-                XmlElement childrenElement = context.CreateElement("Children");
-                this.SerializeItems(node.GetChildren().Cast<Node>(), typeof(Node)).ForEach(childElement => childrenElement.AppendChild(context.ImportNode(childElement, true)));
+                element = context.CreateElement("Generic");
+                element.SetAttribute("Type", nodeType.FullName);
+            }
+            else if (nodeType.IsNested)
+            {
+                element = context.CreateElement("Nested");
+                element.SetAttribute("Type", nodeType.FullName);
+            }
+            else
+            {
+                element = context.CreateElement(nodeType.GetDisplayName());
+            }
+            
+            this.defaultSerializer.SerializeMembers(instance, nodeType).ForEach(pair => element.AppendChild(context.ImportNode(pair.Item1, true)));
+            
+            Node nodeInstance = (Node)instance;
+            if (nodeInstance.GetChildCount() is 0)
+            {
                 return element;
             }
-            catch (Exception exception) when (exception is not SerializationException)
-            {
-                throw new SerializationException(instance, exception);
-            }
+            
+            XmlElement childrenElement = context.CreateElement("Children");
+            element.AppendChild(childrenElement);
+            this.SerializeItems(((Node)instance).GetChildren().Cast<Node>(), typeof(Node)).ForEach(node => childrenElement.AppendChild(context.ImportNode(node, true)));
+            
+            return element;
         }
         
-        /// <summary>
-        /// Deserializes <paramref name="node"/> into an <see cref="object"/>.
-        /// </summary>
-        /// <param name="node">The <see cref="XmlNode"/> to deserialize.</param>
-        /// <param name="nodeType">The <see cref="Type"/> of <see cref="object"/> to deserialize the node as. It must be or inherit from <see cref="Node"/>.</param>
-        /// <returns>An <see cref="object"/> that represents the serialized data stored in <paramref name="node"/>.</returns>
-        /// <exception cref="SerializationException">Thrown if <paramref name="node"/> could not be deserialized due to unexpected errors or invalid input.</exception>
-        public override object? Deserialize(XmlNode node, Type? nodeType = null)
+        public override object Deserialize(XmlNode node, Type? nodeType = null)
         {
-            XmlNode? childrenElement = node.SelectSingleNode("Children");
-            if (childrenElement is null)
-            {
-                return this.ItemSerializer.Deserialize(node, nodeType);
-            }
-            
             nodeType ??= node.GetTypeToDeserialize() ?? throw new SerializationException(node, $"No {nameof(Type)} found to instantiate");
             if (!typeof(Node).IsAssignableFrom(nodeType))
             {
                 throw new SerializationException(node, $"\"{nodeType.GetDisplayName()}\" cannot be (de)serialized by {typeof(NodeSerializer).GetDisplayName()}");
             }
             
-            childrenElement = node.RemoveChild(childrenElement);
+            XmlNode? childrenElement = node.SelectSingleNode("Children");
+            if (childrenElement is not null)
+            {
+                node.RemoveChild(childrenElement);
+            }
             
-            try
+            object instance = Activator.CreateInstance(nodeType, true) ?? throw new SerializationException(node, $"Unable to instantiate {nodeType.GetDisplayName()}");
+            foreach ((object? value, MemberInfo member) in this.defaultSerializer.DeserializeMembers(node, nodeType))
             {
-                object? instance = this.ItemSerializer.Deserialize(node);
-                if (instance is Node nodeInstance)
+                switch (member)
                 {
-                    this.DeserializeItems(childrenElement, typeof(Node))
-                        .Cast<Node>()
-                        .ForEach(child => nodeInstance.AddChild(child));
+                    case PropertyInfo property:
+                        property.SetValue(instance, value);
+                        break;
+                    case FieldInfo field:
+                        field.SetValue(instance, value);
+                        break;
                 }
-                return instance;
             }
-            catch (Exception exception) when (exception is not SerializationException)
+            
+            if (childrenElement is not null)
             {
-                throw new SerializationException(node, exception);
+                Node nodeInstance = (Node)instance;
+                this.DeserializeItems(childrenElement, typeof(Node))
+                    .Cast<Node>()
+                    .ForEach(child => nodeInstance.AddChild(child));
             }
+            
+            return instance;
         }
     }
 }
